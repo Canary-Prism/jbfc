074b3addcc4e89bc662f4686b82d7aed1abb9822
Canary-Prism <canaryprsn@gmail.com>
2025-09-05 15:02:56 +0800

    exports :3

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,14 @@
 module canaryprism.jbfc {
     requires info.picocli;
     uses canaryprism.jbfc.optimise.Optimisation;
+    
+    exports canaryprism.jbfc;
+    exports canaryprism.jbfc.bf;
+    exports canaryprism.jbfc.optimise;
+    exports canaryprism.jbfc.optimise.collapse;
+    exports canaryprism.jbfc.optimise.flow;
+    exports canaryprism.jbfc.optimise.state;
+    
     provides canaryprism.jbfc.optimise.Optimisation with
             canaryprism.jbfc.optimise.collapse.CollapseOptimisation,
             canaryprism.jbfc.optimise.flow.FlowOptimisation,
